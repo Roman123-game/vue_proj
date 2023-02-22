bd51c0d3d3d8226e042e8f46cb0d567ae2f9acec
Roman <romanb@noviopus.com>
2023-02-22 16:40:18 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
-  <h2>Post List</h2>
-  <div>
-    <MYButton @clcick="showDialog">Crete Post</MYButton>
-    <MYSelect v-model="selectedSort" :options="sortOptions" />
-  </div>
+  <div class="app">
+    <h2>Post List</h2>
+    <div>
+      <MYInput v-model="searchQuery" placeholder="Search ..." />
+      <MYButton @clcick="showDialog">Crete Post</MYButton>
+      <MYSelect v-model="selectedSort" :options="sortOptions" />
+    </div>
 
-  <MYDialog v-model:show="dialogVisible">
-    <PostForm @create="createPost" />
-  </MYDialog>
-  <PostList v-bind:posts="posts" @remove="removePost" v-if="!isPostLoading" />
-  <div v-else>Post Loading ...</div>
+    <MYDialog v-model:show="dialogVisible">
+      <PostForm @create="createPost" />
+    </MYDialog>
+    <PostList v-bind:posts="sortedAndSearched" @remove="removePost" v-if="!isPostLoading" />
+    <div v-else>Post Loading ...</div>
+    <div class="page_wrapper">
+      <div v-for = "pageNumber in totalPage" 
+      :key="pageNumber" 
+      class="page"
+      :class="{'current-page': page === pageNumber }"
+      >
+      {{ pageNumber }}
+    </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -19,15 +31,20 @@ import MYButton from './components/UI/MYButton.vue';
 import MYDialog from './components/UI/MYDialog.vue';
 import axios from 'axios';
 import MYSelect from './components/UI/MYSelect.vue';
+import MYInput from './components/UI/MYInput.vue';
 
 export default {
-  components: { PostForm, PostList, MYDialog, MYButton, MYSelect },
+  components: { PostForm, PostList, MYDialog, MYButton, MYSelect, MYInput },
   data() {
     return {
       posts: [],
       dialogVisible: false,
       isPostLoading: false,
       selectedSort: "",
+      searchQuery: "",
+      pageNumber: 1,
+      limit: 10,
+      totalPage: 0,
       sortOptions: [
         { value: 'title', name: "about name" },
         { value: 'body', name: "about body" },
@@ -51,7 +68,14 @@ export default {
     async fetchPosts() {
       try {
         this.isPostLoading = true
-        const responce = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+        const responce = 
+        await axios.get('https://jsonplaceholder.typicode.com/posts',{
+          params:{
+            _page : this.page,
+            _limit: this.limit
+          }
+        });
+        this.totalPage =Math.ceil(responce.headers['x-total-count'] / this.limit),
         this.posts = responce.data
       } catch (e) {
         alert(e)
@@ -60,17 +84,21 @@ export default {
 
       }
     }
-  },
+  },  
   mounted() {
     this.fetchPosts()
   },
-  computed:{
-sortedPosts(){
-  return[...this.posts].sort((post1,post2)=>{
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) => {
         return post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
-
       })
-}
+    },
+    sortedAndSearched() {
+      return this.sortedPosts.filter(post=>
+      post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
+
+    }
   },
   // watch: {
   //   selectedSort(newValue) {
@@ -79,7 +107,7 @@ sortedPosts(){
 
   //     })
   //   },
-  
+
   // }
 }
 
@@ -95,5 +123,19 @@ sortedPosts(){
 
 .app {
   padding: 20px;
+}
+.page_wrapper{
+  display: flex;
+  margin-top: 15px;
+  margin-bottom: 15px;
+  color:black
+
+}
+.page{
+  border: 1px solid lightslategray;
+  padding: 10px;
+}
+.current-page{
+  border: 2px solid lightcoral;
 }
 </style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,13 +1,14 @@
 <template>
   <div v-show="posts.length>0">
     <h4>Post List</h4>
+    <TransitionGroup name="list">
   <PostItem 
   v-for="post in posts" 
   v-bind:key="post.id"
   :post="post"
   @remove="$emit('remove',post)"
   />
- 
+</TransitionGroup>
 </div>
 <h1 v-show ="posts.length ===  0">
   Post List empty
@@ -29,6 +30,22 @@ import PostItem from './PostItem.vue';
 
 <style lang="css" scoped>
 
+.list-move, /* apply transition to moving elements */
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
 
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+   animations can be calculated correctly. */
+.list-leave-active {
+  position: absolute;
+}
 
 </style>
